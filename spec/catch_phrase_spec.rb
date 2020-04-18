62993a0296350566a8b967534ca20adad795b17e
Cailie Skelton <cskelton707@gmail.com>
2020-04-18 00:04:53 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,6 +1,7 @@
 phrase = "It's-a me, Mario!"
-def catch_phrase("It's-a me, Mario!")
+def catch_phrase()
   puts "It's-a me, Mario!"
+  phrase = "It's a me, Mario@"
 end
 
 catch_phrase("It's-a me Mario!")
