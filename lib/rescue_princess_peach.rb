2e0351c6c2c4b7da4ea3c319781d78ac5b780415
Cailie Skelton <cskelton707@gmail.com>
2020-04-18 00:50:33 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
  
 def rescue_princess_peach
-  puts "Hooray! Mario has rescued Princess Peach").to_stdout
-    expect{puts status}.to raise_error(NameError)
-  end
+  puts "Hooray! Mario has rescued Princess Peach"
 end
+
+rescue_princess_peach
